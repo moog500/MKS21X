613807cbcdff03f348f420e339439e77a2148307
Dan AKim <dakim@stuy.edu>
2015-12-16 13:04:45 -0500

    made a driver

---
+++ LabGUI/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+public static void main(String[] args) {
+CToFWindow g = new CToFWindow();
+//ActionEvent e = new ActionEvent());
+g.setVisible(true);
+//g.actionPerformed(e);
+} 
+}
